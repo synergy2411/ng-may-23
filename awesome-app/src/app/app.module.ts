88eb081acafa2243cfad35ef06182c0016efdda6
Sumit Khandelwal <sumitkhandelwal@INDELNB968.ap.bm.net>
2023-05-17 10:53:43 +0530

    nested comp comm

--- awesome-app/src/app/app.module.ts
+++ awesome-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { UserImageComponent } from './users/user-image/user-image.component';
+import { UserInfoComponent } from './users/user-info/user-info.component';
 
 @NgModule({
   declarations: [
     // declare Component, Directive, Pipes before using it
     AppComponent,
     UsersComponent,
+    UserImageComponent,
+    UserInfoComponent,
   ],
   imports: [
     // Import the modules - FormsModule, HttpClientModule, RouterModule etc
---
+++ awesome-app/src/app/users/user-image/user-image.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user-image',
+  templateUrl: './user-image.component.html',
+  styleUrls: ['./user-image.component.css'],
+})
+export class UserImageComponent {
+  @Input() user: any;
+
+  @Output() childEvent = new EventEmitter<string>();
+
+  onBtnClick() {
+    this.childEvent.emit(this.user.company);
+  }
+}
