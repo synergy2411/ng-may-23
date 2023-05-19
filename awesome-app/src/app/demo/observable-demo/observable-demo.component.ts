75f8a9d1c2b39ece56d2edfbd72a937005dd6941
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-05-19 13:50:13 +0530

    operators added

--- awesome-app/src/app/demo/observable-demo/observable-demo.component.ts
+++ awesome-app/src/app/demo/observable-demo/observable-demo.component.ts
@@ -1,22 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Observable, Subscription, interval, of, from, fromEvent } from 'rxjs';
+
+import {
+  debounceTime,
+  filter,
+  map,
+  mergeAll,
+  mergeMap,
+  take,
+  takeUntil,
+  takeWhile,
+} from 'rxjs/operators';
+
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-observable-demo',
   templateUrl: './observable-demo.component.html',
   styleUrls: ['./observable-demo.component.css'],
 })
-export class ObservableDemoComponent implements OnInit {
-  // source$ = interval(1000);
+export class ObservableDemoComponent implements OnInit, AfterViewInit {
+  @ViewChild('btnGenerate') btn: ElementRef;
+  @ViewChild('inputText') inputText: ElementRef;
+
+  repos: { id: number; name: string }[];
+
+  ngAfterViewInit(): void {
+    let inputSource$ = fromEvent(this.inputText.nativeElement, 'input');
+
+    inputSource$
+      .pipe(
+        debounceTime(2000),
+        map((event: any) => event.target.value),
+        mergeMap((val) => {
+          return ajax.getJSON(`https://api.github.com/users/${val}/repos`);
+        })
+        // mergeAll()
+      )
+      .subscribe((data: any[]) => {
+        let transformedVal = data.map((val) => {
+          let transformedValue = { id: val.id, name: val.name };
+          return transformedValue;
+        });
+        this.repos = transformedVal;
+      });
+
+    // .subscribe((obs2) => {
+    //   obs2.subscribe((data: any[]) => {
+    //     let transformedVal = data.map((val) => {
+    //       let transformedValue = { id: val.id, name: val.name };
+    //       return transformedValue;
+    //     });
+    //     this.repos = transformedVal;
+    //   });
+    // });
+
+    let eventSource$ = fromEvent(this.btn.nativeElement, 'click');
+
+    this.source$.pipe(
+      filter((val) => val % 2 == 0),
+      take(10)
+    );
+    // .subscribe(console.log);
+
+    // eventSource$.subscribe(console.log);
+  }
+
+  source$ = interval(1000);
 
   // source$ = of('Hello', 'World');
 
   // source$ = from(['Apple', 'Banana', 'Cat']);
 
-  source$ = fromEvent(document, 'click');
+  // source$ = fromEvent(document, 'click');
 
   ngOnInit(): void {
-    this.source$.subscribe(console.log);
+    // this.source$.pipe(take(5)).subscribe(console.log);
+    // this.source$.pipe(takeWhile((val) => val <= 3)).subscribe(console.log);
   }
 
   unSub$: Subscription;
