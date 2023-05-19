8d832f3f5a593816f5609fd72a9a10bf177bfb32
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-05-19 13:12:08 +0530

    common creational operators

--- awesome-app/src/app/demo/observable-demo/observable-demo.component.ts
+++ awesome-app/src/app/demo/observable-demo/observable-demo.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription, interval, of, from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo',
   templateUrl: './observable-demo.component.html',
   styleUrls: ['./observable-demo.component.css'],
 })
-export class ObservableDemoComponent {
+export class ObservableDemoComponent implements OnInit {
+  // source$ = interval(1000);
+
+  // source$ = of('Hello', 'World');
+
+  // source$ = from(['Apple', 'Banana', 'Cat']);
+
+  source$ = fromEvent(document, 'click');
+
+  ngOnInit(): void {
+    this.source$.subscribe(console.log);
+  }
+
+  unSub$: Subscription;
+
   obs$ = new Observable((observer) => {
+    console.log('Inside observable');
+
     setTimeout(() => {
       observer.next('First Package');
     }, 1000);
@@ -15,13 +31,27 @@ export class ObservableDemoComponent {
       observer.next('Second Package');
     }, 3000);
     setTimeout(() => {
+      observer.error(new Error('Somethign went wrong'));
+    }, 5000);
+    setTimeout(() => {
+      observer.next('Third Package');
+    }, 6000);
+    setTimeout(() => {
       observer.complete();
-    }, 4000);
+    }, 8000);
+  });
+
+  promise = new Promise((resolve, reject) => {
+    console.log('Inside Promise');
+
+    setTimeout(() => {
+      resolve('Promise executed');
+    }, 1000);
   });
 
   onSubscribe() {
     console.log('STARTED');
-    this.obs$.subscribe({
+    this.unSub$ = this.obs$.subscribe({
       next: (data) => {
         console.log(data);
       }, // data handler
@@ -33,5 +63,9 @@ export class ObservableDemoComponent {
       }, // completion
     });
     console.log('ENDED');
+  }
+
+  onUnsubscribe() {
+    this.unSub$.unsubscribe();
   }
 }
